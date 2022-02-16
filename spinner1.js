08b053d08d48fc16774812f99d39e72983c7debc
justinmurphycalgary <justinmurphyis@gmail.com>
2022-02-15 18:46:05 -0700

    repeat spinner

--- spinner1.js
+++ spinner1.js
@@ -1,25 +1,21 @@
-console.clear()
+for (let i = 0; i < 1000; i++) {
+  setInterval(function () {
+    setTimeout(() => {
+      process.stdout.write("\r|   ");
+    }, 50);
 
-for (let i = 0; i < 10000; i++) {
-  setTimeout(() => {
-    process.stdout.write('\r|   ');
-  }, 100);
-  
-  setTimeout(() => {
-    process.stdout.write('\r/   ');
-  }, 300);
-  
-  setTimeout(() => {
-    process.stdout.write('\r-   ');
-  }, 500);
-  
-  setTimeout(() => {
-    // Need to escape the backslash since it's a special character.
-    process.stdout.write('\r\\   '); 
-  }, 700); 
+    setTimeout(() => {
+      process.stdout.write("\r/   ");
+    }, 100);
+
+    setTimeout(() => {
+      process.stdout.write("\r-   ");
+    }, 150);
+
+    setTimeout(() => {
+      // Need to escape the backslash since it's a special character.
+      process.stdout.write("\r\\   ");
+    }, 200),
+      250;
+  });
 }
-
-
-
-
-
